113e337a96e54ed548cde1568032736110b50f6e
trevorwinser <64448847+trevorwinser@users.noreply.github.com>
2025-01-29 02:15:09 -0800

    Add Jump Functionality

--- RollABall/Assets/Scenes/BallController.cs
+++ RollABall/Assets/Scenes/BallController.cs
@@ -3,10 +3,10 @@ using UnityEngine.Events;
 
 public class BallController : MonoBehaviour
 {
-    public UnityEvent<Vector2> OnMove = new UnityEvent<Vector2>();
+    //public UnityEvent<Vector2> OnMove = new UnityEvent<Vector2>();
     [SerializeField] private Rigidbody sphereRigidbody;
-    public float ballSpeed = 2f;
-    public float jumpForce = 1f;
+    public float ballSpeed = 0.01f;
+    public float jumpForce = 0.01f;
     bool isGrounded = false;
     void Start()
     {
@@ -47,20 +47,21 @@ public class BallController : MonoBehaviour
         }
         
 
-        OnMove?.Invoke(inputVector); // When is this used?
+        //OnMove?.Invoke(inputVector); // When is this used?
 
         MoveBall(inputVector);
 
 
         if (Input.GetKey(KeyCode.Space) && isGrounded)
         {
+            sphereRigidbody.linearVelocity = Vector3.zero;
             sphereRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
     }
     public void MoveBall(Vector2 input)
     {
         Vector3 inputXZPlane = new(input.x, 0, input.y);
-        sphereRigidbody.linearVelocity = new Vector3(inputXZPlane.x * ballSpeed, sphereRigidbody.linearVelocity.y, inputXZPlane.z * ballSpeed);
+        sphereRigidbody.AddForce(inputXZPlane * ballSpeed);
     }
 
 
